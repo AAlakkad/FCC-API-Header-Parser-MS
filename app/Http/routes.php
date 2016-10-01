6ac75a8fa058d4bc9a86a67d8955952e6c0abd99
Ammar Alakkad <ammar@Ammars-MacBook-Pro.local>
2016-10-01 10:45:57 +0300

    Add application logic

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -9,8 +9,15 @@
 | It is a breeze. Simply tell Lumen the URIs it should respond to
 | and give it the Closure to call when that URI is requested.
 |
-*/
+ */
 
-$app->get('/', function () use ($app) {
-    return $app->version();
+$app->get('/', function (Illuminate\Http\Request $request) use ($app) {
+    $headers = $request->header();
+    preg_match("/\(([^\)]*)\)/", $headers['user-agent'][0], $software);
+
+    return [
+        'ipaddress' => $request->ip(),
+        'language'  => explode(',', $headers['accept-language'][0])[0],
+        'software'  => $software[1],
+    ];
 });
